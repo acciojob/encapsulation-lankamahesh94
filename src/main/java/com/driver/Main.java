00fc1c8890444a2488ac1128408df3f044d42ac3
lanka <lankamahesh94@gmail.com>
2025-01-17 21:59:36 +0530

    solved Assignment encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly rwonly = new RWOnly();
+        rwonly.setName("Mahi");
+        //System.out.println(rwonly.name);
+        System.out.println(rwonly.getName());
+    }
   
 }
